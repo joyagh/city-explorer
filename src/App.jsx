7d6b369a5c7215226376f40e0d6230f24bc8cb6d
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-08-10 19:57:14 -0500

    fixing bug in movies component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from "axios";
 import { Form, Card } from "react-bootstrap";
-// import movies from "./assets/Movies"
+import Movies from "./assets/Movies";
+
+
 
 const url = import.meta.env.VITE_BACKENDURL || "http://localhost:4001";
 const mapKey = import.meta.env.VITE_MAP_API_KEY;
-console.log(mapKey);
+console.log(mapKey)
 
 class App extends React.Component {
   constructor(props) {
@@ -15,7 +17,8 @@ class App extends React.Component {
       lat: "",
       lon: "",
       img: "",
-      movies: [],
+      movieData: [], // https://image.tmdb.org/t/p/w500
+      weatherData: [],
     };
   }
 componentDidMount(){
@@ -40,7 +43,7 @@ componentDidMount(){
         location,
         lat,
         lon,
-        // movies,
+        
       });
       result = await axios.get(
         `https://maps.locationiq.com/v3/staticmap?key=${mapKey}&center=${lat},${lon}&zoom=<zoom>&size=<width>x<height>&format=<format>&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`,
@@ -54,7 +57,7 @@ componentDidMount(){
       this.setState({
         img: `data:image/png;base64,${base64String}`,
       });
-      this.setState({ location: location });
+      this.setState({location});
     } catch (error) {
       console.error({
         error: "Unable to geocode",
@@ -63,10 +66,17 @@ componentDidMount(){
     try{
       let weatherData = await axios.get(`${url}/weather?lat=${this.state.lat}&lon=${this.state.lon}`)
       console.log(weatherData);
+      this.setState({weatherData: weatherData.data});
     } catch(error){
       console.error({
         error: "Unable to retrieve weather data"
       })
+    }
+    try {
+      let movieData = await axios.get(`${url}/movies?cityname=${this.state.cityName}`)
+     this.setState({movieData: movieData.data})
+ } catch(error) {
+      console.error(error.error)
     }
   };
   render() {
@@ -77,8 +87,15 @@ componentDidMount(){
           <Card.Text>{this.state.lat}</Card.Text>
           <Card.Text>{this.state.lon}</Card.Text>
         </Card>
-        <img src={this.state.img} />
-        {console.log(this.state.img, "line 55")}
+        <div className="movie-card">
+        {this.state.movieData.map((movie, index) => (
+        <Movies key={index} title={movie.title} description={movie.overview} avgVotes={movie.average} totalVotes={movie.total} popularity={movie.popularity} release={movie.release}  />
+         ))};
+
+        </div>
+        TODO weather component...
+        <img src={this.state.img} alt="Weather"/>
+         
         <Form onSubmit={this.handleName}>
           <Form.Label>
             <Form.Control id="city-name" type="text" name="cityName" />
@@ -86,7 +103,7 @@ componentDidMount(){
           <button type="submit">Explore!</button>
         </Form>
       </>
-    );
+    )
   }
 }
 
--- src/assets/Movies.jsx
+++ src/assets/Movies.jsx
@@ -1,1 +1,28 @@
-import R
+import React, { Component } from 'react';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+
+export default class Movies extends Component {
+     constructor(props) {
+          super(props);
+     }
+     
+    
+  render() {
+    return (
+     <div>
+     <Card>
+      <Card.Title>{this.props.title}</Card.Title>
+      <img src={this.props.imgUrl} alt={`Poster Image of the movie ${this.props.title}`} />
+      <ListGroup>
+          <ListGroup.Item>Description: {this.props.overview}</ListGroup.Item>
+          <ListGroup.Item>Average Votes: {this.props.average}</ListGroup.Item>
+          <ListGroup.Item>Total Votes: {this.props.total}</ListGroup.Item>
+          <ListGroup.Item> Popularity: {this.props.popularity}</ListGroup.Item>
+          <ListGroup.Item>Release Date: {this.props.release}</ListGroup.Item>
+      </ListGroup>
+      </Card>
+      </div>
+    )};
+
+
+
